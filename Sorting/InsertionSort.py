54abdeff7cfaac26c3ec05c6232ca0e7aa0c0565
mutal09 <taejja09@gmail.com>
2019-09-10 18:10:42 +0900

    Added HW template

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,15 @@
+# insertion sort template
+
+
+def insertion_sort(l: list) -> list:
+    sorted_list = []
+    ##### YOUR CODE HERE #####
+    raise NotImplementedError
+    ##########################
+    return sorted_list
+
+
+if __name__ == "__main__":
+    l = [3, 7, 8, 5, 2, 1, 5, 9, 4]
+    l = insertion_sort(l)
+    print(l)
---
+++ Sorting/SelectionSort.py
@@ -0,0 +1,15 @@
+# selection sort template
+
+
+def selection_sort(l: list):
+    ##### YOUR CODE HERE #####
+    # selection sort는 실제로 list를 안에서 바꾸면 되기 때문에
+    # 따로 리턴을 할 필요가 없습니다
+    raise NotImplementedError
+    ##########################
+
+
+if __name__ == "__main__":
+    l = [3, 7, 8, 5, 2, 1, 5, 9, 4]
+    selection_sort(l)
+    print(l)
