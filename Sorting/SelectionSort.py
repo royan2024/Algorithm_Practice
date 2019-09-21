d96ae8fdf544330618e2a855a1debe028680f433
mutal09 <taejja09@gmail.com>
2019-09-21 16:21:07 +0900

    added find_min_number / removed current implementation

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,26 +1,28 @@
 # selection sort template
 
 
+def find_min_number(l: list) -> int:
+    m = l[0]
+    for a in l:
+        if m > a:
+            m = a
+
+    return m
+
+
 def selection_sort(l: list):
     sorted_list = []
     i = 1
     while i < len(l):
-        number = 0
-        for a in l:
-            if a > number:
-                sorted_list.append(a)
-            else:
-                sorted_list.insert(0, a)
-            a = number
+        # find min number
+        # remove from original list
+        # insert to sorted list
+        i = i + 1
 
-
-    ##### YOUR CODE HERE #####
-    # selection sort는 실제로 list를 안에서 바꾸면 되기 때문에
-    # 따로 리턴을 할 필요가 없습니다
-    ##########################
-
+    return sorted_list
 
 if __name__ == "__main__":
     l = [3, 7, 8, 5, 2, 1, 5, 9, 4]
+    print(find_min_number(l))
     selection_sort(l)
     print(l)
