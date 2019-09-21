2f4fa1306e2d611f0042a7531a500a22c61b1a63
royan2024 <ean2024@gmail.com>
2019-09-21 16:44:36 +0900

    Add finding min number

--- Sorting/InsertionSort.py
+++ Sorting/InsertionSort.py
@@ -8,6 +8,14 @@ def insertion_sort(l: list) -> list:
         x = l[i]
         # TODO: x가 sorted list의 어디에 들어가야 하나?
         # how-1: sorted list를 앞에서부터 보고, x보다 큰 값이 나오는 위치에 x를 insert
+        idx = len(sorted_list)
+        for j in range(len(sorted_list)):
+            y = sorted_list[j]
+            if y > x:
+                idx = j
+                break
+        sorted_list.insert(idx, x)
+        print(sorted_list)
         # how-2: sorted list를 binary search하고 맞는 위치에 x를 insert
         i = i + 1
 
