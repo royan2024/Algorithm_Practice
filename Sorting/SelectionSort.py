a0a6bd8df6c59fac40b89e1f1b60c1424bbeaddd
mutal09 <taejja09@gmail.com>
2019-09-21 16:36:59 +0900

    implemented in-place selection sort

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -22,7 +22,16 @@ def selection_sort(l: list):
 
     return sorted_list
 
+
+def selection_sort_in_place(l: list):
+    for i in range(len(l)):
+        m = i
+        for j in range(i, len(l)):
+            if l[m] > l[j]:
+                m = j
+        l[m], l[i] = l[i], l[m]
+
 if __name__ == "__main__":
     l = [3, 7, 8, 5, 2, 1, 5, 9, 4]
-    l = selection_sort(l)
+    selection_sort_in_place(l)
     print(l)
